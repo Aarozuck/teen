1059ed94e8d83c3444464ad6763f2c55c8197557
Aarozuck <132171616+Aarozuck@users.noreply.github.com>
2023-09-16 00:30:22 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
 
-pip install -r requirements.txt
+echo "Installing dependencies..."
 
+python3.9 -m pip install -r requirements.txt
 
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+echo "Migrating database..."
+
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collecting static files..."
+
+python3.9 manage.py collectstatic --noinput
