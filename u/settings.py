7247c611e3ad2cf7baf04ce495e01977dcf6fd72
Aarozuck <132171616+Aarozuck@users.noreply.github.com>
2023-10-14 23:36:59 +0300

    Update settings.py

--- u/settings.py
+++ u/settings.py
@@ -139,3 +139,7 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
+ 
